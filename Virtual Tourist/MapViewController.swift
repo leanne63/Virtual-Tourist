2bf840a86a03dad80e5556a8fe72c1b7ebe44198
leanne63 <leanne63@users.noreply.github.com>
2016-07-25 14:29:19 -0500

    moved load code into private utility function

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -26,41 +26,7 @@ class MapViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		// retrieve initial values for map region
-		var centerLatitude: CLLocationDegrees = mapView.region.center.latitude
-		var centerLongitude: CLLocationDegrees = mapView.region.center.longitude
-		var spanLatitudeDelta: CLLocationDegrees = mapView.region.span.latitudeDelta
-		var spanLongitudeDelta: CLLocationDegrees = mapView.region.span.longitudeDelta
-
-		
-		// if haven't launched previously, this key will not exist; however iOS will report its value as 'false'
-		let hasLaunchedPreviously = NSUserDefaults.standardUserDefaults().boolForKey(hasLaunchedKey)
-		
-		if hasLaunchedPreviously {
-			// retrieve stored map values from last use
-			centerLatitude = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapCenterLatitudeKey)
-			centerLongitude = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapCenterLongitudeKey)
-			spanLatitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapSpanLatitudeDeltaKey)
-			spanLongitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapSpanLongitudeDeltaKey)
-		}
-		else {
-			// since hasn't launched previously, create the "has launched" value now
-			NSUserDefaults.standardUserDefaults().setBool(true, forKey: hasLaunchedKey)
-
-			// store current map region values for use on next startup
-			NSUserDefaults.standardUserDefaults().setDouble(centerLatitude, forKey: MapModel.mapCenterLatitudeKey)
-			NSUserDefaults.standardUserDefaults().setDouble(centerLongitude, forKey: MapModel.mapCenterLongitudeKey)
-			NSUserDefaults.standardUserDefaults().setDouble(spanLatitudeDelta, forKey: MapModel.mapSpanLatitudeDeltaKey)
-			NSUserDefaults.standardUserDefaults().setDouble(spanLongitudeDelta, forKey: MapModel.mapSpanLongitudeDeltaKey)
-		}
-		
-		// set map's region
-		let centerCoordinate = CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude)
-		let spanValues = MKCoordinateSpan(latitudeDelta: spanLatitudeDelta, longitudeDelta: spanLongitudeDelta)
-		
-		let region = MKCoordinateRegionMake(centerCoordinate, spanValues)
-
-		mapView.region = region
+		mapView.region = getMapRegion()
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -87,6 +53,48 @@ class MapViewController: UIViewController {
 		
 		// head on over to next view
 		performSegueWithIdentifier(photoAlbumSegueID, sender: self)
+	}
+	
+	
+	// MARK: - Private Utility Functions
+	
+	private func getMapRegion() -> MKCoordinateRegion {
+		
+		// retrieve initial values for map region
+		var centerLatitude: CLLocationDegrees = mapView.region.center.latitude
+		var centerLongitude: CLLocationDegrees = mapView.region.center.longitude
+		var spanLatitudeDelta: CLLocationDegrees = mapView.region.span.latitudeDelta
+		var spanLongitudeDelta: CLLocationDegrees = mapView.region.span.longitudeDelta
+		
+		
+		// if haven't launched previously, this key will not exist; however iOS will report its value as 'false'
+		let hasLaunchedPreviously = NSUserDefaults.standardUserDefaults().boolForKey(hasLaunchedKey)
+		
+		if hasLaunchedPreviously {
+			// retrieve stored map values from last use
+			centerLatitude = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapCenterLatitudeKey)
+			centerLongitude = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapCenterLongitudeKey)
+			spanLatitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapSpanLatitudeDeltaKey)
+			spanLongitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(MapModel.mapSpanLongitudeDeltaKey)
+		}
+		else {
+			// since hasn't launched previously, create the "has launched" value now
+			NSUserDefaults.standardUserDefaults().setBool(true, forKey: hasLaunchedKey)
+			
+			// store current map region values for use on next startup
+			NSUserDefaults.standardUserDefaults().setDouble(centerLatitude, forKey: MapModel.mapCenterLatitudeKey)
+			NSUserDefaults.standardUserDefaults().setDouble(centerLongitude, forKey: MapModel.mapCenterLongitudeKey)
+			NSUserDefaults.standardUserDefaults().setDouble(spanLatitudeDelta, forKey: MapModel.mapSpanLatitudeDeltaKey)
+			NSUserDefaults.standardUserDefaults().setDouble(spanLongitudeDelta, forKey: MapModel.mapSpanLongitudeDeltaKey)
+		}
+		
+		// set map's region
+		let centerCoordinate = CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude)
+		let spanValues = MKCoordinateSpan(latitudeDelta: spanLatitudeDelta, longitudeDelta: spanLongitudeDelta)
+		
+		let region = MKCoordinateRegionMake(centerCoordinate, spanValues)
+		
+		return region
 	}
 
 }
