98c83c6c7401b163893c2c0b1ea4711e1394746b
leanne63 <leanne63@users.noreply.github.com>
2016-08-07 12:56:20 -0500

    refactored core data stack to Apple original, add saves to app delegate

--- Virtual Tourist/CoreDataStack.swift
+++ Virtual Tourist/CoreDataStack.swift
@@ -2,7 +2,7 @@
 //  CoreDataStack.swift
 //  Virtual Tourist
 //
-//	Provided by Udacity,
+//	Provided by Apple,
 //  Modified by leanne on 7/26/16.
 //  Copyright © 2016 leanne63. All rights reserved.
 //
@@ -10,25 +10,25 @@
 import Foundation
 import CoreData
 
-struct CoreDataStack {
+class CoreDataStack {
 	
-	static var applicationDocumentsDirectory: NSURL = {
-		// The directory the application uses to store the Core Data store file. This code uses a directory named "com.leanne63.Virtual_Tourist" in the application's documents Application Support directory.
+	lazy var applicationDocumentsDirectory: NSURL = {
+		// The directory the application uses to store the Core Data store file. This code uses a directory named "com.leanne63.THROWAWAY" in the application's documents Application Support directory.
 		let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
 		return urls[urls.count-1]
 	}()
 	
-	static var managedObjectModel: NSManagedObjectModel = {
+	lazy var managedObjectModel: NSManagedObjectModel = {
 		// The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-		let modelURL = NSBundle.mainBundle().URLForResource("Virtual_Tourist", withExtension: "momd")!
+		let modelURL = NSBundle.mainBundle().URLForResource("THROWAWAY", withExtension: "momd")!
 		return NSManagedObjectModel(contentsOfURL: modelURL)!
 	}()
 	
-	static var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+	lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
 		// The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
 		// Create the coordinator and store
-		let coordinator = NSPersistentStoreCoordinator(managedObjectModel: CoreDataStack.managedObjectModel)
-		let url = CoreDataStack.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+		let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+		let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
 		var failureReason = "There was an error creating or loading the application's saved data."
 		do {
 			try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
@@ -49,9 +49,9 @@ struct CoreDataStack {
 		return coordinator
 	}()
 	
-	static var managedObjectContext: NSManagedObjectContext = {
+	lazy var managedObjectContext: NSManagedObjectContext = {
 		// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-		let coordinator = CoreDataStack.persistentStoreCoordinator
+		let coordinator = self.persistentStoreCoordinator
 		var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
 		managedObjectContext.persistentStoreCoordinator = coordinator
 		return managedObjectContext
@@ -59,10 +59,10 @@ struct CoreDataStack {
 	
 	// MARK: - Core Data Saving support
 	
-	static func saveContext () {
-		if CoreDataStack.managedObjectContext.hasChanges {
+	func saveContext () {
+		if managedObjectContext.hasChanges {
 			do {
-				try CoreDataStack.managedObjectContext.save()
+				try managedObjectContext.save()
 			} catch {
 				// Replace this implementation with code to handle the error appropriately.
 				// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
