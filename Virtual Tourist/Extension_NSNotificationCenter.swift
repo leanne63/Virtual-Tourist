dbf7c328d80c7a3c5810bd932070cbd4a1b10062
leanne63 <leanne63@users.noreply.github.com>
2016-08-14 00:31:27 -0500

    fixed leftover merge issues

---
+++ Virtual Tourist/Extension_NSNotificationCenter.swift
@@ -0,0 +1,47 @@
+//
+//  Extension_NSNotificationCenter.swift
+//  Virtual Tourist
+//
+//  Created by leanne on 8/13/16.
+//  Copyright © 2016 leanne63. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotificationCenter {
+	
+	/**
+	
+	Creates and posts a notification to the main thread.
+	
+	- parameters:
+	- notificationName: Notification name to be provided to observers.
+	- userInfo: Dictionary of custom information to be provided to observers, or nil if none needed.
+	
+	*/
+	class func postNotificationOnMain(notificationName: String, object: AnyObject?, userInfo: [String: AnyObject]?) {
+		
+		let notification = NSNotification(name: notificationName, object: nil, userInfo: userInfo)
+		
+		NSOperationQueue.mainQueue().addOperationWithBlock {
+			NSNotificationCenter.defaultCenter().postNotification(notification)
+		}
+	}
+	
+	
+	/**
+	
+	Post notification containing a failure message.
+	
+	- parameter failureMessage: Failure information to be provided to observers.
+	
+	*/
+	class func postFailureNotification(notificationName: String, object: AnyObject?, failureMessage: String) {
+		
+		let userInfo = ["failureMessage": failureMessage]
+		
+		NSNotificationCenter.postNotificationOnMain(notificationName, object: object, userInfo: userInfo)
+	}
+	
+	
+}
--- Virtual Tourist/Flickr.swift
+++ Virtual Tourist/Flickr.swift
@@ -1,5 +1,4 @@
 //
-<<<<<<< HEAD
 //  Flickr.swift
 //  Virtual Tourist
 //
